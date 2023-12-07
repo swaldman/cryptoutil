aa3a2fca3e84d5ee4e38d6fa174a14b5af91bcee
Steve Waldman <swaldman@mchange.com>
2023-12-06 19:27:24 -0500

    Hash.SHA3_256: Add withHexBytes(...) methods, replace ambiguous apply() with hash()

--- src/com/mchange/cryptoutil/Hash.scala
+++ src/com/mchange/cryptoutil/Hash.scala
@@ -5,9 +5,13 @@ import scala.collection.immutable
 
 object Hash:
   object SHA3_256:
-    def apply[T : Byteable](t : T) : SHA3_256 =
+    def hash[T : Byteable](t : T) : SHA3_256 =
       val md = MessageDigest.getInstance("SHA3-256") // see https://docs.oracle.com/en/java/javase/11/docs/specs/security/standard-names.html#messagedigest-algorithms
       immutable.ArraySeq.unsafeWrapArray( md.digest( t.toByteArray ) )
+    def withHexBytes( hex : String ) : SHA3_256 =
+      hex.decodeHexToSeq
+        .ensuring( _.length == 32, "A Hash.SHA3_256 has must contain exactly 32 bytes. '${hex}' is not the correct length.")
+
   opaque type SHA3_256 <: Hash = immutable.Seq[Byte]
 
   extension ( h : Hash )
@@ -15,7 +19,7 @@ object Hash:
     def toSeq       : Seq[Byte]   = h
     def hex         : String      = _hex( h.toArray )
     def hex0x       : String      = _hex0x( h.toArray )
-    
+
 
 opaque type Hash = immutable.Seq[Byte]
 
